cea41cd12a7d0452e24ccac3f7437d3490a89614
Simone Dos Santos Silva <110856273+SimoneSS15@users.noreply.github.com>
2022-12-11 07:24:57 -0300

    Add files via upload

---
+++ comentario.js
@@ -0,0 +1,10 @@
+// console.log é usado para exibir algo na tela do computador
+// o ponto e virgula no final da sentença não é obrigatório
+
+console.log("Passo #3");
+console.log("Passo #1");
+//console.log("Passo #2"); linha comentada
+/*Esse
+comentário é
+de múltiplas linhas
+ */
---
+++ sentenca_1.js
@@ -0,0 +1,5 @@
+console.log("Bom dia!");
+console.log("Bom tarde!");
+console.log("Bom noite!");
+
+// lembrar de usar a extensão code runner e configurar a maneira de salvar e executar os arquivos
